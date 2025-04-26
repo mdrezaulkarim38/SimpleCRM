cb147e991d7644093845ab080e8a9a76b897e3c9
Rezaul Karim <mdrezaulkarim31295@gmail.com>
2025-04-26 21:40:44 +0600

    update project

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using SimpleCRM.Data;
-using SimpleCRM.Services;
-using System.Text;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,47 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Register TokenService for dependency injection
-builder.Services.AddScoped<TokenService>(provider =>
-{
-    var configuration = provider.GetRequiredService<IConfiguration>();
-    var secretKey = configuration["JwtSettings:SecretKey"];
-    var issuer = configuration["JwtSettings:Issuer"];
-    var audience = configuration["JwtSettings:Audience"];
-    return new TokenService(secretKey, issuer, audience);
-});
-
-// Configure JWT Authentication
-var jwtSettings = builder.Configuration.GetSection("JwtSettings");
-var key = Encoding.ASCII.GetBytes(jwtSettings["SecretKey"]);
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = "JwtBearer";
-    options.DefaultChallengeScheme = "JwtBearer";
-})
-.AddJwtBearer("JwtBearer", options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = jwtSettings["Issuer"],
-        ValidAudience = jwtSettings["Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ClockSkew = TimeSpan.Zero // Prevent delay in token expiration
-    };
-});
-
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Enable Authentication & Authorization Middleware
-app.UseAuthentication();
-app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -66,6 +26,7 @@ else
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapDefaultControllerRoute();
 app.Run();
